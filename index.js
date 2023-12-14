32aa9044b7e22210fbf8696e198fee14110971cb
PIeter <pieter_ophalvens@hotmail.com>
2023-12-14 16:32:14 +0100

    info page and seperated key check

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 import recipe from "./recipe.js";
 import express, { json } from "express";
 import "dotenv/config";
-const api = process.env.API_KEY;
+const API = process.env.API_KEY;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(json());
 const checkApiKey = (req, res, next) => {
-  if (true) next();
+  if (req.params.api == API) next();
+  else res.status(403).send(`incorrect api key!`);
 };
-app.get("/", checkApiKey, (req, res) => {
-  res.send({ code: 200, msg: "ok" });
-});
 
 app.get("/", (req, res) => {
   res.send({ code: 200, msg: "ok" });
 });
-
-app.post("/login", (req, res) => {
-  res.status(403);
+app.get("/info", (req, res) => {
+  res.send(`Welcome to the 'food'API ! \r\n enter your api-key in the url /mykey/ \n
+    specify where youd like to go( /posts ) and the id or name youd like to retrieve /hugo \n
+    it should look something like http://localhost:3000/2007/posts/hugo`);
 });
-
 app.get("/posts", (req, res) => {
   res.send(recipe);
 });
-app.post("/posts/new", (req, res) => {
+app.post("/posts/new", checkApiKey, (req, res) => {
   const newPost = {
     name: req.body.name,
     time: req.body.time,
@@ -33,14 +30,13 @@ app.post("/posts/new", (req, res) => {
   };
   recipe.push(newPost);
 });
-app.get(`/${api}/posts/:name`, (req, res) => {
-  console.log(req.params.name, api);
+app.get(`/:api/posts/:name`, checkApiKey, (req, res) => {
   let post = [];
   const id = parseInt(req.params.name);
   if (id <= recipe.length && id >= 0) post = recipe[id];
   else post = recipe.find((post) => post.name === req.params.name);
-  if (post == undefined) res.send("post not found!");
-  res.send(post);
+  if (post == undefined) res.status(404).send("post not found!");
+  else res.send(post);
 });
 
 app.listen(PORT, () => {
